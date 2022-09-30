3d30809bea9b214711d534246f88ffa0eda7e99f
Ningen26 <ricabu1112@gmail.com>
2022-09-30 17:32:03 -0400

    First Commit

---
+++ src/RecursionLab/ArraySum.java
@@ -0,0 +1,10 @@
+package RecursionLab;
+
+public class ArraySum {
+    public int sumOfArray (Integer[] a,int index)
+    {
+        if(index <= 0)
+            return a[0];
+        return  sumOfArray(a,index-1) + a[index];
+    }
+}
